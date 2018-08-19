cbeac4e1e6e908e501b5f2db2f0400b687ec08fd
Abhi Agarwal <Abhi_Agarwal@Dell.com>
2018-08-19 17:13:28 +0530

    New files

---
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
+
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+    animations: [routerTransition()]
+})
+export class DashboardComponent implements OnInit {
+    public alerts: Array<any> = [];
+    public sliders: Array<any> = [];
+    public data : any = []; 
+    temp : any = [];
+    constructor() {  
+        var i: number;
+        for(var i = 1;i<=5;i++)
+        {
+            this.temp.FirstName = "Dhanraj";
+            this.temp.LastName = "Kumar";
+            this.temp.Grade = i;
+            this.temp.Career = "Career Cluster Inventory";
+            this.temp.RealityCheck = "Reality Check";
+            this.data.push(this.temp);
+        }   
+        this.sliders.push(
+            {
+                imagePath: 'assets/images/slider1.jpg',
+                label: 'First slide label',
+                text:
+                    'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+            },
+            {
+                imagePath: 'assets/images/slider2.jpg',
+                label: 'Second slide label',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            },
+            {
+                imagePath: 'assets/images/slider3.jpg',
+                label: 'Third slide label',
+                text:
+                    'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+            }
+        );
+
+        this.alerts.push(
+            {
+                id: 1,
+                type: 'success',
+                message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+            },
+            {
+                id: 2,
+                type: 'warning',
+                message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+            }
+        );
+    }
+
+    ngOnInit() {}
+
+    public closeAlert(alert: any) {
+        const index: number = this.alerts.indexOf(alert);
+        this.alerts.splice(index, 1);
+    }
+
+    SearchButtonClicked()
+    {
+        alert("asda");
+    }
+}
