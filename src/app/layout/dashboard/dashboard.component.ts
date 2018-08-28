128e876e3e18fcab7a054ea6f2909f12281ffd50
dhanrajsmit <dhanrajsmit007@gmail.com>
2018-08-28 18:40:10 +0530

    excel

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -4,135 +4,183 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { PagerService } from '../../_services';
 import {DashboardapiService} from '../../shared/services/dashboardapiservice/dashboardapi.service';
-
+import {ExcelService} from '../../../app/shared/services/ExcelService/excel.service';
 @Component({
-    moduleId: module.id,
-    selector: 'app-root',
-    styleUrls: ['./dashboard.component.scss'],
-    templateUrl: 'dashboard.component.html'
+   moduleId: module.id,
+   selector: 'app-root',
+   styleUrls: ['./dashboard.component.scss'],
+   templateUrl: 'dashboard.component.html'
 })
 
 export class DashboardComponent implements OnInit {
-    constructor(private http: Http, private pagerService: PagerService,public dashBoarApiService :DashboardapiService) { }
-    queryString: any;
-    pageSize: number;
-    currentPage: number;
-    limitOptions = [
-        {
-          key: '5',
-          value: 5
-        },
-        {
-          key: '10',
-          value: 10
-        },
-        {
-          key: '20',
-          value: 20
-        }
-      ];
+   constructor(private http: Http, private pagerService: PagerService,public dashBoarApiService :DashboardapiService,private excelService:ExcelService) { }
+   queryString: any;
+   pageSize: number;
+   currentPage: number;
+   limitOptions = [
+       {
+         key: '5',
+         value: 5
+       },
+       {
+         key: '10',
+         value: 10
+       },
+       {
+         key: '20',
+         value: 20
+       }
+     ];
 
-    // array of all items to be paged
-    allItems: any[];
+   // array of all items to be paged
+   allItems: any[];
 
-    allitemsTemp = [];
+   allitemsTemp = [];
 
-    // pager object
-    pager: any = {};
+   allitemsPDF = [];
+   Pdfdata = [];
 
-    // paged items
-    pagedItems: any[];
-    itemList =[];
+   // pager object
+   pager: any = {};
 
-    ngOnInit() {
-         this.pageSize = 10;
-         this.dashBoarApiService.dashBoardApiHandler().subscribe(data => {
-            console.log(data);
-            this.allItems = data.json().Result.defaultPlanProgressReport.UserActivityItemCompletionStatusList;
-           
-           //console.log("check------>"+this.allItems)
-           
-            this.allitemsTemp = this.allItems;
-            this.setPage(1);
-        });
-    
-         
-    }
+   // paged items
+   pagedItems: any[];
+   itemList =[];
 
-    defaultPlanProgressReport() {
-      
-        // console.log(this.allItems);
-         this.allItems.map((item) =>{
-           var UserActivityItemCompletionStatusList ={
-               ActivityCompleted: '',
-               DisplayName: '',
-               CurrentGradeLevel: '',
-               FirstName: '',
-               MiddleName: '',
-               LastName: '',
- 
-           };
-           UserActivityItemCompletionStatusList.ActivityCompleted=item.ActivityCompleted;
-           UserActivityItemCompletionStatusList.DisplayName=item.DisplayName;
-           UserActivityItemCompletionStatusList.CurrentGradeLevel=item.CurrentGradeLevel;
-           UserActivityItemCompletionStatusList.FirstName=item.FirstName;
-           UserActivityItemCompletionStatusList.MiddleName=item.MiddleName;
-           UserActivityItemCompletionStatusList.LastName=item.LastName;
- 
-           this.itemList.push(UserActivityItemCompletionStatusList);
-           // console.log(this.itemList);
-         })
-     
- };
- 
-
-    setPage(page: number) {
+   ngOnInit() {
         this.pageSize = 10;
+        this.dashBoarApiService.dashBoardApiHandler().subscribe(data => {
+           console.log(data);
+           this.allItems = data.json().Result.defaultPlanProgressReport.UserActivityItemCompletionStatusList;
 
-        this.currentPage = page;
-       // alert(this.currentPage);
+          //console.log("check------>"+this.allItems)
 
-        // get pager object from service
-        this.pager = this.pagerService.getPager(this.allitemsTemp.length, page);
-       // alert("start "+this.pager.startIndex + "end Index" +this.pager.endIndex);
+           this.allitemsTemp = this.allItems;
+           this.allitemsPDF = this.allItems;
 
-
-        // get current page of items
-        this.pagedItems = this.allitemsTemp.slice(this.pager.startIndex, this.pager.endIndex + 1);
-    }
-
-    onPageSizeChanged(event) {
-        this.pageSize = event;
-        this.pagerService.setPageLimit(event);
-
-        this.setPage(1);
-
-      }
-
-      searchString() {
-          // tslint:disable-next-line:prefer-const
-          let queryStringVar = this.queryString;
-          try {
-       this.allitemsTemp = this.allItems.filter((item) => {
-        // tslint:disable-next-line:prefer-const
-        let studentName:string= item.FirstName+item.MiddleName+item.LastName;    
-        console.log("searchString item = "+studentName+" queryStringVar = "+queryStringVar);
-        
-          if (studentName.toLowerCase().includes(queryStringVar.toLowerCase())) {
-            console.log('searchString1 filter title includes = ' + studentName.includes(queryStringVar));
-
-              return item;
-          }
+           this.setPage(1);
 
        });
-      console.log('searchString1 alltempitemslength= ' + this.allitemsTemp.length);
 
-            this.setPage(1);
-          }
-          catch (e) {
-              alert('searchString  ex ception ' + e.message);
-          }
+
+   }
+
+   defaultPlanProgressReport() {
+
+    // console.log(this.allItems);
+     this.allItems.map((item) =>{
+       var UserActivityItemCompletionStatusList ={
+           ActivityCompleted: '',
+           DisplayName: '',
+           CurrentGradeLevel: '',
+           FirstName: '',
+           MiddleName: '',
+           LastName: '',
+
+       };
+       UserActivityItemCompletionStatusList.ActivityCompleted=item.ActivityCompleted;
+       UserActivityItemCompletionStatusList.DisplayName=item.DisplayName;
+       UserActivityItemCompletionStatusList.CurrentGradeLevel=item.CurrentGradeLevel;
+       UserActivityItemCompletionStatusList.FirstName=item.FirstName;
+       UserActivityItemCompletionStatusList.MiddleName=item.MiddleName;
+       UserActivityItemCompletionStatusList.LastName=item.LastName;
+
+       this.itemList.push(UserActivityItemCompletionStatusList);
+       // console.log(this.itemList);
+     })
+
+};
+
+
+setPage(page: number) {
+    this.pageSize = 10;
+
+    this.currentPage = page;
+   // alert(this.currentPage);
+
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.allitemsTemp.length, page);
+   // alert("start "+this.pager.startIndex + "end Index" +this.pager.endIndex);
+
+
+    // get current page of items
+    this.pagedItems = this.allitemsTemp.slice(this.pager.startIndex, this.pager.endIndex + 1);
+}
+
+onPageSizeChanged(event) {
+    this.pageSize = event;
+    this.pagerService.setPageLimit(event);
+
+    this.setPage(1);
+
+  }
+
+  searchString() {
+      // tslint:disable-next-line:prefer-const
+      let queryStringVar = this.queryString;
+      try {
+   this.allitemsTemp = this.allItems.filter((item) => {
+    // tslint:disable-next-line:prefer-const
+    let studentName:string= item.FirstName+item.MiddleName+item.LastName;
+    console.log("searchString item = "+studentName+" queryStringVar = "+queryStringVar);
+
+      if (studentName.toLowerCase().includes(queryStringVar.toLowerCase())) {
+        console.log('searchString1 filter title includes = ' + studentName.includes(queryStringVar));
+
+          return item;
       }
-     
+
+   });
+  console.log('searchString1 alltempitemslength= ' + this.allitemsTemp.length);
+
+
+  this.allitemsPDF = this.allitemsTemp;
+
+        this.setPage(1);
+      }
+      catch (e) {
+          alert('searchString  ex ception ' + e.message);
+      }
+  }
+
+
+  exportAsXLSX():void {
+    alert("check")
+
+    var allitemsPDFList ={
+      name:'',
+      CurrentGradeLevel: '',
+      DisplayName:'',
+      ActivityCompleted:'',
+   };
+
+
+    // for (let index = 0; index < this.allitemsPDF.length; index++) {
+
+    //   allitemsPDFList.name = this.allitemsPDF.name
+
+
+
+    // }
+
+// this.Pdfdata.push(allitemsPDFList);
+
+
+console.log("this.allitemsPDF------>"+this.allitemsPDF.length);
+console.log("this.allitemsPDF------>"+JSON.stringify(this.allitemsPDF));
+
+
+
+    this.excelService.exportAsExcelFile(this.allitemsPDF, 'sample');
+  }
+
+  exportAsPDF(){
+
+  }
+
+
+
+
+
 
 }
